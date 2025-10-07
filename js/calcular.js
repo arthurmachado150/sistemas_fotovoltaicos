902a6a99c379c0ad9a76094670398684dc36fbd8
Arthur <arthur_machado150@estudante.sesisenai.org.br>
2025-10-07 16:48:06 -0300

    Quarta versão

--- js/calcular.js
+++ js/calcular.js
@@ -16,6 +16,6 @@ function principal(){
     console.log(`Você precisará de: ${calculo_de_quantas_placas_sao_necessarias.toFixed(2)} placas solares Tiger Pro 72HC Monofacial <br>`)
 
     resposta.innerHTML = ``
-    resposta.innerHTML += `Se o valor em kWh/mês desejado é: ${kWh_mes_desejada} kWh/mês <br>`
+    resposta.innerHTML += `Se o valor em kWh/mês desejado é: ${kWh_mes_desejada} kWh/mês <br><br>`
     resposta.innerHTML += `Você precisará de: ${calculo_de_quantas_placas_sao_necessarias.toFixed(2)} placas solares (Tiger Pro 72HC Monofacial) <br>`
 }
